c881462fa1f84036a31ab5534e0fb33463b7162d
caroleastier <carole.astier@gmail.com>
2021-12-10 16:39:23 +0100

    Ajout Athlete routeur POST et formulaire

--- routers/athlete.router.js
+++ routers/athlete.router.js
@@ -5,8 +5,30 @@ const AthleteController = require('../controllers/athlete.controller');
 const athleteController = new AthleteController();
 
 router.get('/athletes/', async (req, res) => {
-    athleteController.list(req, res);
+    await athleteController.list(req, res);
 });
 
+router.get('/createAthlete', (req, res) => {
+    res.render('addAthlete');
+});
+
+router.post ('/createAthlete', function(req,res) {
+    console.log(req.body);
+    let item = {
+        lastName: req.body.lastName,
+        firstName: req.body.firstName,
+        age: req.body.age,
+        country: req.body.country,
+    };
+    mongoose.connect(uri, function (err, db) {
+        //assert.equal(null, err);
+        db.collection('Athlete').insertOne(item, function (err, result) {
+            //assert.equal(null, err);
+            console.log('item has been inserted');
+            db.close;
+        });
+    });
+    res.redirect("/");
+});
 
 module.exports = router;
